b2b6280ae91aa01cb552d992836d9b6f24b494d8
Ritik Kumar <ritikkraj066@gmail.com>
2024-08-17 01:05:27 +0530

    ffv

--- django_project/settings.py
+++ django_project/settings.py
@@ -22,7 +22,8 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
+# ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
+ALLOWED_HOSTS = ["sens-sphere.onrender.com","localhost","127.0.0.1"]
 
 
 # Quick-start development settings - unsuitable for production
